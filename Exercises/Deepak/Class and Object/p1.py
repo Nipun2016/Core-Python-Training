4a6faeb4ec1859bcbad2cd5b65dd095a18ebe3a3
deepak895 <dac81048@gmail.com>
2017-01-18 13:02:16 +0530

    Data Added

--- Exercises/Deepak/Class and Object/p1.py
+++ Exercises/Deepak/Class and Object/p1.py
@@ -1,13 +1,18 @@
 class Basic(object):
     #initialize the variables
-    def __init__(self, arg):
+    def __init__():
+        print("Constructor Called...")
+        
+    #get Data From User
+    def get_String(self, arg):
         self.arg = arg
 
     #Print the variable of Class
-    def print_data(self):
+    def print_String(self):
         return self.arg.upper()
 
+obj = Basic(word)
 #takes input from User
 word = input("Enter String : ")
-obj = Basic(word)
-print (obj.print_data())
+obj.get_String(word)
+print (obj.print_String())
