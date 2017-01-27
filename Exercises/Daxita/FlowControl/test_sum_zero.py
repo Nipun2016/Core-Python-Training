436c1acb96dd05d1e246e940e3ba583a1c825940
daxita <daxita2013@gmail.com>
2017-01-27 16:32:20 +0530

    Delete test_sum_zero.py

--- Exercises/Daxita/FlowControl/test_sum_zero.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from sum_zero import sum_zero
-
-class PrintStrTestCase(unittest.TestCase):
-    def test_sum_zero(self):
-    	S=sum_zero()
-    	self.assertEqual(S.compute([2,3,4,6,0,-1,-6]),[[2, 4, -6], [6, 0, -6]])
-if __name__=="__main__":
-	unittest.main()
