d926cfa2dcbe8282c4ded00938357745ee9e8113
daxita <daxita2013@gmail.com>
2017-01-27 16:31:00 +0530

    Delete test_count_object.py

--- Exercises/Daxita/FlowControl/test_count_object.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from count_object import count
-
-class CountTestCase(unittest.TestCase):
-	def test_count_object(self):
-		c=count()
-		self.assertEqual(count.counter,1)
-if __name__=="__main__":
-	unittest.main()
